df34ac374e4069e5955e117793f2a097db7e756c
DominicJW <96926484+DominicJW@users.noreply.github.com>
2023-11-11 23:28:23 +0000

    Update dns_challenge.sh

--- dns_challenge.sh
+++ dns_challenge.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-$filename = "/etc/bind/zones/db.$CERTBOT_DOMAIN"
+filename="/etc/bind/zones/db.$CERTBOT_DOMAIN"
 sed -i '/_acme-challenge/d' $filename
-$newline = "_acme-challenge    IN     TXT   \"$CERTBOT_VALIDATION\""
+newline="_acme-challenge    IN     TXT   \"$CERTBOT_VALIDATION\""
 echo $newline >> $filename
 sudo systemctl restart bind9
 sleep 25
